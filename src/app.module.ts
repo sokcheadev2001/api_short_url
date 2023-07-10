e65f2b0e0082c6f73b9f364078a44c8a85285499
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-07-10 20:58:15 +0700

    Cleanup project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { EnvironmentModule } from './config/env/environment.module';
 @Module({
-  imports: [],
+  imports: [EnvironmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
