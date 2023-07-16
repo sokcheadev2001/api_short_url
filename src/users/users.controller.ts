fee30de379df146eb86bf42e1ee2d7d9f0da7fa3
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-07-16 13:33:57 +0700

    feature: user register

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,18 +6,20 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Req() request: Request, @Body() createUserDto: CreateUserDto) {
+    return await this.usersService.create(request.ip, createUserDto);
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+  async create(clientIp: string, createUserDto: CreateUserDto) {
+    // Handle password hashing
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    const newUser = new User();
+    newUser.username = createUserDto.username;
+    newUser.email = createUserDto.email;
+    newUser.password = hashPassword;
+    newUser.ip = clientIp;
+    return this.usersRepository.save(newUser);
   }
 
   findAll() {
