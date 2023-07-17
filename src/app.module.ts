0ed967bbd3327dfe8fdae961c7f2203ea1e7ad6d
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-07-17 14:30:11 +0700

    generate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { EnvironmentModule } from './config/env/environment.module';
 import { DatabaseModule } from './config/database/database.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [EnvironmentModule, DatabaseModule, UsersModule],
+  imports: [EnvironmentModule, DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
