14e644ffb38ef0c74d964ffa31c46060ee6ee18b
sokcheadev2001 <sokchealeang.dev@gmail.com>
2023-07-19 12:03:32 +0700

    enable cors with client side

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.APP_CORS_ORIGIN as string,
+  });
   app.useGlobalPipes(new ValidationPipe());
+  console.log(process.env.APP_CORS_ORIGIN);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
